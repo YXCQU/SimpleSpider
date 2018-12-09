76157597a7da788f4dd9acf53c6332aaac8ee182
YXCQU <326080508@qq.com>
2018-12-09 17:17:28 +0800

    调整项目结构

--- proxy/mimvp_proxy.py
+++ proxy/mimvp_proxy.py
@@ -2,31 +2,68 @@ import requests
 from config import get_header
 from bs4 import BeautifulSoup
 
-order_id = '861080148271221207'
-url = f'https://proxyapi.mimvp.com/api/fetchsecret.php?orderid={order_id}&num=5&http_type=3&result_fields=1,2,3'
-s = 'https://proxyapi.mimvp.com/api/fetchopen.php?orderid=861080148271221207&num=20&http_type=2&ping_time=1&transfer_time=5&check_success_count=100&filter_hour=12&result_fields=1,2&result_format=json'
-free_url = "https://proxy.mimvp.com/free.php"  # free url
-free_params = ['in_hp', 'in_socks', 'out_socks']
+order_id = '864050900873190210'
+# url_https = f'https://proxyapi.mimvp.com/api/fetchopen.php?orderid={order_id}&num=20&anonymous=3,5&ping_time=5&transfer_time=10&check_success_count=100&result_fields=1,2,5,6,7,8&result_format=json'
+# url_https = 'https://proxyapi.mimvp.com/api/fetchsecret.php?orderid=864050900873190210&num=5&result_fields=1,2,3&result_format=json'
+url_https = 'https://proxyapi.mimvp.com/api/fetchopen.php?orderid=864050900873190210&num=20&result_fields=1,2&result_format=json&ping_time=5&transfer_time=10'
 
 
 # 付费米扑代理
-def get_mimvp():
+def get_mp_http(_url=None):
     """
     从接口获得代理IP
     :return:
     """
     try:
-        r = requests.get(url, timeout=8)
+        r = requests.get(_url, timeout=8)
     except TimeoutError:
         print("代理服务器超时")
     res = r.json()
     all_ip = res['result']
+    print(all_ip)
 
-    for ip in all_ip:
-        yield ip['ip:port']
+    for _ip in all_ip:
+        ip = ''
+        if 'HTTP' in _ip['http_type']:
+            ip = 'http://' + _ip['ip:port']
+        elif 'Socks4' in _ip['http_type']:
+            ip = 'Socks4://' + _ip['ip:port']
+        elif 'Socks5' in _ip['http_type']:
+            ip = 'socks5://' + _ip['ip:port']
+        yield ip
+
+
+def proxy(func=None, url=None):
+    """
+    格式化 ip 为 requests 格式
+    :param func:
+    :param url:
+    :return:
+    """
+
+    for ip in func(url):
+        requests_template = {
+            "https": "",
+            "http": ""
+        }
+        if "http" in ip:
+            requests_template['https'] = ip
+            requests_template['http'] = ip
+        elif "socks4" in ip:
+            requests_template['https'] = ip
+            requests_template['http'] = ip
+        elif "socks5" in ip:
+            requests_template['https'] = ip
+            requests_template['http'] = ip
+        yield requests_template
+
+
+def get_proxy():
+    return proxy(get_mp_http, url_https)
 
 
 if __name__ == '__main__':
     # res = get_proxy()
     # print(res)
-    get_mimvp()
+    for proxy in proxy(get_mp_http, url_https):
+        print(proxy)
--- spider/tianyancha/money.py
+++ spider/tianyancha/money.py
@@ -1,53 +1,27 @@
 import requests
 from config import *
 from lxml import etree
-import time
-import json
-import random
+from proxy.mimvp_proxy import get_proxy
 
 
-def get_money(key):
-    ips = []
-    with open('ip.txt', 'r') as f:
-        ips = [i for i in f.readlines()]
-    index = random.randint(0, len(ips) - 1)
-    proxy = {
-        'https': 'https://%s' % ips[index].replace('\n', ''),
-        'http': 'http://%s' % ips[index].replace('\n', '')
-    }
-    url = 'https://www.tianyancha.com/search?key={}'
-    try_num = 3
+def get_money(key, proxy=None):
+    url = f'http://m.tianyancha.com/search?key={key}'
+    try_num = 1
     while try_num > 0:
         try:
-            r = requests.get(url.format(key), headers=tyc_headers, timeout=6)
+            print(proxy)
+            r = requests.get(url, headers=tyc_headers, timeout=10, proxies=proxy)
             html = etree.HTML(r.text)
             # print(r.text)
-            result = html.xpath('//*[@id="web-content"]/div/div[1]/div/div[3]/div/div/div[2]/div[2]/div[2]/span/text()')[0]
+            result = html.xpath('//div[@class="search_row_new_mobil"]/div[1]/div[2]/span')[0].text
             print(result)
             return result
         except Exception as e:
             print(e)
-            index = random.randint(0, len(ips) - 1)
-            proxy = {
-                'https': 'https://%s' % ips[index].replace('\n', ''),
-                'http': 'http://%s' % ips[index].replace('\n', '')
-            }
             try_num -= 1
             continue
 
 
 if __name__ == '__main__':
-    file_name = 'excel1.txt'
-    with open(file_name, 'r') as f:
-        for line in f.readlines():
-            res = ' '
-            try:
-                res = get_money(line)
-                if not res:
-                    res = ' '
-            except:
-                res = ' '
-            finally:
-                with open('res_' + file_name, 'a') as f1:
-                    f1.write(res)
-                    f1.write('\n')
+    for proxy in get_proxy():
+        get_money('editorai', proxy)
